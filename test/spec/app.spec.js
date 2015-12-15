d33b2e38839cdd46b7af497d24d32361b5d0f9b8
Tentello <prempaolo@gmail.com>
2015-12-15 17:18:56 +0100

    sprint1

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -21,6 +21,48 @@ describe("Santa", function() {
 }];
         SantaModel.init(request);
         expect(SantaModel.getCurrentRequest().question).toBe("Carlo wants a TOY. Shall I pack a banana?");
+  }); 
+    
+      it("should pack properly", function() {  
+        var request = [{
+  question : "Carlo wants a TOY. Shall I pack a banana?",
+  options : ["yes", "no"],
+  answer : "no"
+},
+{
+  question : "Julia wants a doll. Shall I pack a barbie?",  
+  options : ["yes", "no"],
+  answer : "yes"
+},
+{
+  question : "Fabio wants a smartphone. Shall I pack a potatoe?",  
+  options : ["yes", "no"],
+  answer : "no"
+}];
+        SantaModel.init(request);
+        expect(SantaModel.pack("no")).toBe(1);
   });    
+    
+          it("should not pack properly", function() {  
+        var request = [{
+  question : "Carlo wants a TOY. Shall I pack a banana?",
+  options : ["yes", "no"],
+  answer : "no"
+},
+{
+  question : "Julia wants a doll. Shall I pack a barbie?",  
+  options : ["yes", "no"],
+  answer : "yes"
+},
+{
+  question : "Fabio wants a smartphone. Shall I pack a potatoe?",  
+  options : ["yes", "no"],
+  answer : "no"
+}];
+        SantaModel.init(request);
+        expect(SantaModel.pack("yes")).toBe(0);
+  }); 
  
+ 
+
 });
