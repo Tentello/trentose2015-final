c2a41f7d6857a7cb62488675d76fbf822e7a454f
Tentello <prempaolo@gmail.com>
2015-12-15 17:14:29 +0100

    sprint1

--- js/app.js
+++ js/app.js
@@ -2,13 +2,38 @@
  * It provides quality control by checking if the requests are being
  * fulfilled
  */
+var SantaView = {
+    init : function(request){
+        $(".question").append(request.question);
+        for(var i=0; i<request.options.length; i++){
+            $(".question-items").append("<li id='"+request.options[i]+"'>"+request.options[i]+"</li>");
+        }
+        
+    }
+};
 
 var SantaModel = {
+    
+    requests : [],
+    
+    currentQuestion : 0,
+    
+    currentAnswer : "",
+    
+    punti : 0,
   
   /* Initializes the model with a list of requests, 
    * and sets the first one as the current one 
    */
    init : function(list){
+       $(".question-items").click(function(){
+           alert($(this).children().attr("id"));
+            if(SantaModel.pack($(this).children().text())==1){
+                alert("right");
+                punti+=1;
+            }
+       });
+       this.requests = list;
    },
   
    /* It moves "current" to the next request */
@@ -19,6 +44,8 @@ var SantaModel = {
     * If all requests have been processed (there is no current one), it returns null 
     */
    getCurrentRequest : function () {
+       SantaModel.currentAnswer = (this.requests[this.currentQuestion]).answer;
+       return this.requests[this.currentQuestion];
    },  
     
    /* Packs the given item if it fulfills the current request.       
@@ -26,6 +53,22 @@ var SantaModel = {
     * returns 0 if the given item does not fulfill the request
     */
    pack : function(item) {
+       if(item == SantaModel.currentAnswer){
+            return 1;
+       } else {
+            return 0;   
+       }
    }      
   
 };
+
+var SantaController = {
+    init : function(){
+        SantaModel.init(requests);
+        SantaView.init(SantaModel.getCurrentRequest());
+    }
+};
+
+$(document).ready(function(){
+    SantaController.init();
+});
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,24 @@
 
 describe("Santa", function() {
 
-  it("should...", function() {  
-    
+  it("should init properly", function() {  
+        var request = [{
+  question : "Carlo wants a TOY. Shall I pack a banana?",
+  options : ["yes", "no"],
+  answer : "no"
+},
+{
+  question : "Julia wants a doll. Shall I pack a barbie?",  
+  options : ["yes", "no"],
+  answer : "yes"
+},
+{
+  question : "Fabio wants a smartphone. Shall I pack a potatoe?",  
+  options : ["yes", "no"],
+  answer : "no"
+}];
+        SantaModel.init(request);
+        expect(SantaModel.getCurrentRequest().question).toBe("Carlo wants a TOY. Shall I pack a banana?");
   });    
  
 });
